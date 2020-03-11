dd28d349148659d81e178fc23b56d1da61d6fb9b
Pierre <49315644+ech1@users.noreply.github.com>
2020-03-11 18:57:21 +0100

    eza
    zea

--- swamp_scripts/swamp.sh
+++ swamp_scripts/swamp.sh
@@ -10,14 +10,14 @@ if [ $? != 0 ]
 then
         echo "|----------Starting Steam...-------------|"
         echo "+----------------------------------------+"
-        sudo -u ech0 /usr/bin/steam %U > /dev/null & sleep 60
+        sudo /bin/sh -e /usr/games/steam > /dev/null & sleep 10
 else 
         echo "|----------Steam already started---------|"
         echo "+----------------------------------------+"
 fi
 
 chmod +x GModCEFCodecFix-Linux && ./GModCEFCodecFix-Linux & sleep 10
-
+cpulimit --path=/home/ech0/.steam/steam/steamapps/common/GarrysMod/bin/linux64/gmod --limit=10 &
 while true
 do
 printf "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
@@ -33,7 +33,7 @@ then
         echo "+----------------------------------------+"
         echo "|-----Connection lost, restarting...-----|"
         echo "+----------------------------------------+"
-        /bin/sh -e /usr/games/steam -applaunch 4000 -dev -console +connect 208.103.169.51:27015 -windowed -w 1265 -h 716 & sleep 10
+        /bin/sh -e /usr/games/steam -applaunch 4000 -dev -console +connect 208.103.169.51:27015 -windowed -w 1200 -h 600 & sleep 10
 else
         printf "\n\n\n\n\n"
         echo    "+----------------------------------------+"
@@ -44,3 +44,6 @@ else
         sleep 1 ; netstat -alnp | grep p | grep gmod
         printf "+-------------------------------------------------------------------------------------------------+\n"
 fi
+sleep 10;
+done
+
