95568bef98d967c6d6b8720e640e1d0ed0a81385
Pierre <49315644+ech1@users.noreply.github.com>
2020-03-11 15:54:49 +0100

    adz
    zea

--- swamp_scripts/swamp.sh
+++ swamp_scripts/swamp.sh
@@ -35,6 +35,7 @@ then
         echo "|-----Connection lost, restarting...-----|"
         echo "+----------------------------------------+"
         sudo -u ech0 /usr/bin/steam %U -applaunch 4000 -dev -console +connect 208.103.169.72:27015 -windowed -w 1265 -h 716 -novid -high
+        #UBUNTU : /bin/sh -e /usr/games/steam -applaunch 4000 -dev -console +connect 208.103.169.72:27015 -windowed -w 1265 -h 716
 else
         printf "\n\n\n\n\n"
         echo    "+----------------------------------------+"
