acbfa3b6517ccc2bceaa71f83acff14c37431001
Pierre <49315644+ech1@users.noreply.github.com>
2020-05-21 23:03:25 +0200

    updated linux swamp cinema
    updated linux swamp cinema

--- swamp_scripts/swamp.sh
+++ swamp_scripts/swamp.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 # RUN IT LIKE THAT :
 # wget https://raw.githubusercontent.com/ech1/Random-Scripts/master/swamp_scripts/swamp.sh -O - | bash
+
 gpid=$(pidof gmod)
 echo pid of gmod: $gpid
 while true
 do
 whoami
+sudo whoami
+whoami
+
+echo '0) Idle (minimalistic)'
+echo '1) Idle (semi-minimal)'
+echo '2) active (borderless)'
+echo '3) active (full)'
+read choice
+
 while [ "$(pidof gmod)" -ge 1 ] 2>/dev/null
 do
         echo "---------------------------------gmod running!-----------------------------------"
@@ -19,7 +29,7 @@ do
         echo "-number of udp packets: $udp"
 
         ###########################UDP
-        if [ "$udp" -lt 30 ] 2>/dev/null
+        if [ "$udp" -lt 10 ] 2>/dev/null
         then
                 echo "---NO not enough udps!"
                 kill -9 $(pidof steam)
@@ -28,17 +38,38 @@ do
         fi
 
         ##########################RAM
-        ramusage=$(echo $ramtotal'>'75 | bc -l)
+
+        ramtotal=$(ps -p $(pidof gmod) -o vsize | grep -v VSZ)
+        ramusage=$(echo $ramtotal'>'8000000 | bc -l)
         if [ "$ramusage" -eq 1 ] 2>/dev/null
         then
                 echo '---NO TOO MUCH RAM USED !'
                 kill -9 $(pidof steam)
         else
-                echo '---YES ram usage below 75 !'
+                echo "---YES ram usage is $ramtotal !"
         fi
 done
-echo '-------------------------- gmod not running! 3 secs -----------------------------'
-sleep 3
-#exec /usr/bin/steam -silent -no-browser -applaunch 4000 -windowed -safe -noaddons -nochromium -console +connect cinema.swampservers.net:27015 >/dev/null & 
-exec /usr/bin/steam -silent -applaunch 4000 -windowed -w 1600 -h 900 -noborder -noaddons -nochromium -console +connect cinema.swampservers.net:27015 >/dev/null & 
+echo '-------------------------- gmod not running! 9 secs -----------------------------'
+sleep 9
+
+case "$choice" in
+"0")
+        exec /usr/bin/steam -silent -no-browser -applaunch 4000 +connect cinema.swampservers.net:27015 -windowed -safe -noaddons -nochromium -console >/dev/null &
+        ;;
+"1")
+        exec /usr/bin/steam -silent -no-browser -applaunch 4000 +connect cinema.swampservers.net:27015 -windowed -w 1600 -h 900 -noaddons -nochromium -console >/dev/null &
+        ;;
+"2")
+        exec /usr/bin/steam -silent -applaunch 4000 +connect cinema.swampservers.net:27015 -windowed -w 1600 -h 900  -console >/dev/null &
+        ;;
+"3")
+        exec /usr/bin/steam -silent -applaunch 4000 +connect cinema.swampservers.net:27015 -full -console >/dev/null &
+        ;;
+*)
+        exit 1
+        ;;
+esac
+
+#exec /usr/bin/steam -silent -no-browser -applaunch 4000 -windowed -safe -noaddons -nochromium -console +connect cinema.swampservers.net:27015 >/dev/null &
+#exec /usr/bin/steam -silent -applaunch 4000 -windowed -w 1600 -h 900 -noborder -console +connect cinema.swampservers.net:27015 >/dev/null &
 done
