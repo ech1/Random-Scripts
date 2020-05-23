5d6ee6d56d87790fa2782e2168f3c463089f64c0
Pierre <49315644+ech1@users.noreply.github.com>
2020-05-23 14:56:08 +0200

    check if connected to internet
    check if connected to internet

--- swamp_scripts/swamp.sh
+++ swamp_scripts/swamp.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 # RUN IT LIKE THAT :
-# wget https://raw.githubusercontent.com/ech1/Random-Scripts/master/swamp_scripts/swamp.sh 
-# chmod +x swamp.sh
-# ./swamp.sh
+# wget https://raw.githubusercontent.com/ech1/Random-Scripts/master/swamp_scripts/swamp.sh -O - | bash
 
 gpid=$(pidof gmod)
 echo pid of gmod: $gpid
-while true
-do
 whoami
 sudo whoami
 whoami
@@ -18,22 +14,26 @@ echo '2) active (borderless)'
 echo '3) active (full)'
 read choice
 
+while true
+do
+
 while [ "$(pidof gmod)" -ge 1 ] 2>/dev/null
 do
-        echo "---------------------------------gmod running!-----------------------------------"
+        echo "--------------------- Gmod Running ! -----------------------"
         echo "$(pidof gmod) waiting 30 seconds"
         sleep 30
 
         ramtotal=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
         echo "-total ram: $ramtotal"
 
-        udp=$(sudo timeout 3 tcpdump -u port 27015 2>/dev/null | wc -l)
+        udp=$(sudo timeout 30 tcpdump host 208.103.169.51 2>/dev/null | wc -l)
         echo "-number of udp packets: $udp"
 
         ###########################UDP
         if [ "$udp" -lt 10 ] 2>/dev/null
         then
                 echo "---NO not enough udps!"
+                kill -9 $(pidof gmod)
                 kill -9 $(pidof steam)
         else
                 echo "---YES enough udps!"
@@ -46,32 +46,38 @@ do
         if [ "$ramusage" -eq 1 ] 2>/dev/null
         then
                 echo '---NO TOO MUCH RAM USED !'
+                kill -9 $(pidof gmod)
                 kill -9 $(pidof steam)
         else
                 echo "---YES ram usage is $ramtotal !"
         fi
 done
-echo '-------------------------- gmod not running! 9 secs -----------------------------'
-sleep 9
 
-case "$choice" in
-"0")
-        exec /usr/bin/steam -silent -no-browser -applaunch 4000 +connect cinema.swampservers.net:27015 -windowed -safe -noaddons -nochromium -console >/dev/null &
-        ;;
-"1")
-        exec /usr/bin/steam -silent -no-browser -applaunch 4000 +connect cinema.swampservers.net:27015 -windowed -w 1600 -h 900 -noaddons -nochromium -console >/dev/null &
-        ;;
-"2")
-        exec /usr/bin/steam -silent -applaunch 4000 +connect cinema.swampservers.net:27015 -windowed -w 1600 -h 900  -console >/dev/null &
-        ;;
-"3")
-        exec /usr/bin/steam -silent -applaunch 4000 +connect cinema.swampservers.net:27015 -full -console >/dev/null &
-        ;;
-*)
-        exit 1
-        ;;
-esac
+wget -q --tries=1 --timeout=2 --spider https://swampservers.net
+if [[ $? -eq 0 ]]; then
+        echo '--------------- Internet Connection ON ----------------------'
+        echo '--------------- gmod not running! 15 secs -------------------'
 
-#exec /usr/bin/steam -silent -no-browser -applaunch 4000 -windowed -safe -noaddons -nochromium -console +connect cinema.swampservers.net:27015 >/dev/null &
-#exec /usr/bin/steam -silent -applaunch 4000 -windowed -w 1600 -h 900 -noborder -console +connect cinema.swampservers.net:27015 >/dev/null &
+        case "$choice" in
+        "0")
+                exec /usr/bin/steam -silent -no-browser -applaunch 4000 +connect cinema.swampservers.net:27015 -windowed -safe -noaddons -nochromium -console >/dev/null &
+                ;;
+        "1")
+                exec /usr/bin/steam -silent -no-browser -applaunch 4000 +connect cinema.swampservers.net:27015 -windowed -w 1600 -h 900 -noaddons -nochromium -console >/dev/null &
+                ;;
+        "2")
+                exec /usr/bin/steam -silent -applaunch 4000 +connect cinema.swampservers.net:27015 -windowed -w 1600 -h 900 -noborder -console >/dev/null &
+                ;;
+        "3")
+                exec /usr/bin/steam -silent -applaunch 4000 +connect cinema.swampservers.net:27015 -full -console >/dev/null &
+                ;;
+        *)
+                exit 1
+                ;;
+        esac
+else
+        echo '--------------- Internet Connection OFF ---------------------'
+fi
+sleep 15
 done
+
