7543f3ca55b438dbe5616972e1a65f55870c193a
Pierre <49315644+ech1@users.noreply.github.com>
2020-07-05 11:05:45 +0200

    swamp active script
    swamp active script

---
+++ swamp_scripts/swamp_act.bat
@@ -0,0 +1,30 @@
+@ECHO OFF
+
+
+
+Echo "Swamp Cinema Automation Script"
+GOTO :A
+
+:A
+TASKLIST | FINDSTR /I "steam.exe"
+IF ERRORLEVEL 1 (GOTO :StartSteam) ELSE (ECHO Steam is currently running)
+timeout 60
+GOTO :B
+
+:B
+TASKLIST | FINDSTR /I "gmod.exe"
+IF ERRORLEVEL 1 (GOTO :StartSwamp) ELSE (ECHO Swamp Cinema is currently running)
+timeout 60
+GOTO :B
+
+:StartSwamp
+ECHO "Swamp Cinema closed, Restarting..."
+"C:\Program Files (x86)\Steam\Steam.exe" -applaunch 4000 +connect cinema.swampservers.net:27015 -multirun -high -heapsize 1048576 -windowed -w 1920 -h 1080 -noborder -hijack
+timeout 60
+GOTO :B
+
+:StartSteam
+ECHO "Steam is closed, Restarting..."
+start Steam
+timeout 60
+GOTO :A
