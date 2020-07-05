3bc4f6351a34e1a78fb1b08b84834144dadd7fd4
Pierre <49315644+ech1@users.noreply.github.com>
2020-07-06 00:07:38 +0200

    swamp linux script update
    swamp linux script update

--- swamp_scripts/swamp.sh
+++ swamp_scripts/swamp.sh
@@ -9,7 +9,7 @@ sudo whoami
 whoami
 
 echo '0) Idle (minimalistic)'
-echo '1) Idle (semi-minimal)'
+echo '1) Idle (online)'
 echo '2) active (borderless)'
 echo '3) active (full)'
 read choice
@@ -26,15 +26,15 @@ do
         ramtotal=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
         echo "-total ram: $ramtotal"
 
-	sudo timeout 1 tcpdump
-        udp=$(sudo timeout 30 tcpdump host 208.103.169.51 2>/dev/null | wc -l)
+        sudo timeout 0.1 sudo tcpdump -i enp0s31f6
+        udp=$(sudo timeout 30 tcpdump -i enp0s31f6 host 208.103.169.51 2>/dev/null | wc -l)
         echo "-number of udp packets: $udp"
 
         ###########################UDP
         if [ "$udp" -lt 10 ] 2>/dev/null
         then
                 echo "---NO not enough udps!"
-		kill -9 $(pidof gmod)
+                kill -9 $(pidof gmod)
                 kill -9 $(pidof steam)
         else
                 echo "---YES enough udps!"
@@ -47,7 +47,7 @@ do
         if [ "$ramusage" -eq 1 ] 2>/dev/null
         then
                 echo '---NO TOO MUCH RAM USED !'
-		kill -9 $(pidof gmod)
+                kill -9 $(pidof gmod)
                 kill -9 $(pidof steam)
         else
                 echo "---YES ram usage is $ramtotal !"
@@ -56,28 +56,28 @@ done
 
 wget -q --tries=1 --timeout=10 --spider https://swampservers.net
 if [[ $? -eq 0 ]]; then
-	echo '--------------- Internet Connection ON ----------------------'
-	echo '--------------- gmod not running! 15 secs -------------------'
+        echo '--------------- Internet Connection ON ----------------------'
+        echo '--------------- gmod not running! 15 secs -------------------'
 
-	case "$choice" in
-	"0")
-	        exec /usr/bin/steam -offline -silent -no-browser -applaunch 4000 +connect cinema.swampservers.net:27015 -windowed -w 960 -h 540 -noborder -noaddons -nochromium -console >/dev/null 2>/dev/null &
-	        ;;
-	"1")
-	        exec /usr/bin/steam -silent -no-browser -applaunch 4000 +connect cinema.swampservers.net:27015 -windowed -w 1920 -h 1080 -noborder -noaddons -nochromium -console >/dev/null 2>/dev/null &
-	        ;;
-	"2")
-	        exec /usr/bin/steam -silent -applaunch 4000 +connect cinema.swampservers.net:27015 -windowed -w 1600 -h 900 -noborder -console >/dev/null 2>/dev/null &
-	        ;;
-	"3")
-	        exec /usr/bin/steam -silent -applaunch 4000 +connect cinema.swampservers.net:27015 -windowed -noborder -w 1920 -h 1080 -console >/dev/null 2>/dev/null &
-	        ;;
-	*)
-	        exit 1
-	        ;;
-	esac
+        case "$choice" in
+        "0")
+                exec /usr/bin/steam -tcp -offline -silent -no-browser -applaunch 4000 +connect cinema.swampservers.net:27015 -windowed -safe -noaddons -nochromium -console >/dev/null 2>/dev/null &
+                ;;
+        "1")
+                exec /usr/bin/steam -silent -no-browser -applaunch 4000 +connect cinema.swampservers.net:27015 -windowed -safe -noaddons -nochromium -console >/dev/null 2>/dev/null &
+                ;;
+        "2")
+                exec /usr/bin/steam -silent -applaunch 4000 +connect cinema.swampservers.net:27015 -windowed -w 1600 -h 900 -noborder -console >/dev/null 2>/dev/null &
+                ;;
+        "3")
+                exec /usr/bin/steam -silent -applaunch 4000 +connect cinema.swampservers.net:27015 -windowed -noborder -w 1920 -h 1080 -console >/dev/null 2>/dev/null &
+                ;;
+        *)
+                exit 1
+                ;;
+        esac
 else
-	echo '--------------- Internet Connection OFF ---------------------'
+        echo '--------------- Internet Connection OFF ---------------------'
 fi
 sleep 15
 done
