e3343049ddb3732078948e6b55b41faf998a0b61
ech1 <ech1@nowhere.com>
2020-08-10 19:55:15 +0100

    3hours dirty script for lazy nigs

---
+++ swamp_scripts/Windows/swamp_3hours.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+
+
+Echo "Swamp Cinema Automation Script"
+GOTO :A
+
+:A
+"C:\Program Files (x86)\Steam\Steam.exe" -applaunch 4000 +connect cinema.swampservers.net -textmode -noshaderapi -noaddons -nochromium -low -heapsize 1048576"
+timeout 10800
+taskkill /IM gmod.exe
+GOTO :A
+
