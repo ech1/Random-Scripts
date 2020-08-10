9161310ecf9ee491a27bb13a38387aea1aae1aa8
Pierre <49315644+ech1@users.noreply.github.com>
2020-08-10 18:48:39 +0100

    f
    f

--- swamp_scripts/Windows/swamp_3hours.bat
+++ swamp_scripts/Windows/swamp_3hours.bat
@@ -7,6 +7,6 @@ GOTO :A
 :A
 "C:\Program Files (x86)\Steam\Steam.exe" -applaunch 4000 +connect cinema.swampservers.net -textmode -noshaderapi -noaddons -nochromium -low -heapsize 1048576"
 timeout 10800
-taskkill /IM gmod.exe
+taskkill /IM gmod.exe /F
 GOTO :A
 
