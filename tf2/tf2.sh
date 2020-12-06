90bb6813519f17339bd579be72d62a1c21755f6a
ech1 <ech1@nowhere.com>
2020-12-06 11:26:11 +0100

    yes

---
+++ tf2/tf2.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+
+echo '[+] TF2 script'
+echo '[+] 1) tr_walkway_rc2 	(RED)'
+echo '[+] 2) jump_mireal 	(BLU)'
+echo '[+] 3) mge (onlinewars)'
+echo '[+] 4) mge (panda)'
+echo '[+] 5) trade_minecraft'
+read choice
+
+case $choice in
+	1)
+		/usr/bin/steam -silent -no-browser -applaunch 440 -windowed -w 2560 -h 1440 +sv_allow_point_servercommand always -novid +map workshop/606778917 -noborder -console +viewmodel_fov 130 >/dev/null 2>/dev/null &
+		;;
+	2)
+		/usr/bin/steam -silent -no-browser -applaunch 440 -windowed -w 2560 -h 1440 +map jump_mireal -noborder -console -novid +viewmodel_fov 130 >/dev/null 2>/dev/null &
+		;;
+	3)
+		/usr/bin/steam -silent -no-browser -applaunch 440 -windowed -w 2560 -h 1440 +connect 46.174.48.183:27015 -novid -noborder -console +viewmodel_fov 130 >/dev/null 2>/dev/null &
+		;;
+	4)
+		echo
+		 /usr/bin/steam -silent -no-browser -applaunch 440 -windowed -w 2560 -h 1440 +connect 176.57.142.80:27055 -novid -noborder -console +viewmodel_fov 130 >/dev/null 2>/dev/null &
+		;;
+	5)
+		echo
+		/usr/bin/steam -silent -no-browser -applaunch 440 -windowed -w 2560 -h 1440 +connect 5.135.189.29:27015  -novid -noborder -console +viewmodel_fov 130 >/dev/null 2>/dev/null &
+		;;
+	*)
+		echo 'ERR0R'
+		break
+		;;
+esac
+
+
+#/usr/bin/steam -silent -no-browser -applaunch 440 -windowed -w 2560 -h 1440 +sv_allow_point_servercommand always +map workshop/606778917 -noborder -console +viewmodel_fov 130 >/dev/null 2>/dev/null &
+
+#/usr/bin/steam -silent -no-browser -applaunch 440 -windowed -w 2560 -h 1440 +sv_allow_point_servercommand always +map jump_mireal -noborder -console +viewmodel_fov 130 >/dev/null 2>/dev/null &
+
+#/usr/bin/steam -silent -no-browser -applaunch 440 -windowed -w 2560 -h 1440 +connect -noborder -console +viewmodel_fov 130 >/dev/null 2>/dev/null &
