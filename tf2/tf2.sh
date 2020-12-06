fc55e8f5624825c35c80e8c0b7dd0483692de211
ech1 <ech1@nowhere.com>
2020-12-06 12:07:04 +0100

    tf2 script done

--- tf2/tf2.sh
+++ tf2/tf2.sh
@@ -17,15 +17,15 @@ case $choice in
 		/usr/bin/steam -silent -no-browser -offline -applaunch 440 -windowed -w 2560 -h 1440 +map jump_mireal -noborder -console -novid +viewmodel_fov 130 >/dev/null 2>/dev/null &
 		;;
 	3)
-		/usr/bin/steam -silent -no-browser -offline -applaunch 440 -windowed -w 2560 -h 1440 +connect 46.174.48.183:27015 -novid -noborder -console +viewmodel_fov 130 >/dev/null 2>/dev/null &
+		/usr/bin/steam -silent -no-browser -offline -applaunch 440 -windowed -w 2560 -h 1440 +connect 46.174.48.183:27015 -novid -noborder -console +viewmodel_fov 130 +hud_saytext_time 0 +hud_deathnotice_time 1 +bind u say_team +bind y  >/dev/null 2>/dev/null &
 		;;
 	4)
 		echo
-		 /usr/bin/steam -silent -no-browser -offline -applaunch 440 -windowed -w 2560 -h 1440 +connect 176.57.142.80:27055 -novid -noborder -console +viewmodel_fov 130 >/dev/null 2>/dev/null &
+		 /usr/bin/steam -silent -no-browser -offline -applaunch 440 -windowed -w 2560 -h 1440 +connect 176.57.142.80:27055 -novid -noborder -console +viewmodel_fov 130 +hud_saytext_time 0 +hud_deathnotice_time 1 +bind u say_team +bind y say >/dev/null 2>/dev/null &
 		;;
 	5)
 		echo
-		/usr/bin/steam -silent -no-browser -offline -applaunch 440 -windowed -w 2560 -h 1440 +connect 5.135.189.29:27015  -novid -noborder -console +viewmodel_fov 130 >/dev/null 2>/dev/null &
+		/usr/bin/steam -silent -no-browser -offline -applaunch 440 -windowed -w 2560 -h 1440 +connect 5.135.189.29:27015  -novid -noborder -console +viewmodel_fov 130 +hud_saytext_time 0 +hud_deathnotice_time 1 +unbind u +unbind y >/dev/null 2>/dev/null &
 		;;
 	*)
 		echo 'ERR0R'
