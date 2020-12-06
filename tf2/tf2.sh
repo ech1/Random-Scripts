5b8433d3a89b1e1c6d7f61c23b202b4ac83828d4
ech1 <ech1@nowhere.com>
2020-12-06 11:34:50 +0100

    Aiyeas A

--- tf2/tf2.sh
+++ tf2/tf2.sh
@@ -11,21 +11,21 @@ read choice
 
 case $choice in
 	1)
-		/usr/bin/steam -silent -no-browser -applaunch 440 -windowed -w 2560 -h 1440 +sv_allow_point_servercommand always -novid +map workshop/606778917 -noborder -console +viewmodel_fov 130 >/dev/null 2>/dev/null &
+		/usr/bin/steam -silent -no-browser -offline -applaunch 440 -windowed -w 2560 -h 1440 +sv_allow_point_servercommand always -novid +map workshop/606778917 -noborder -console +viewmodel_fov 130 >/dev/null 2>/dev/null &
 		;;
 	2)
-		/usr/bin/steam -silent -no-browser -applaunch 440 -windowed -w 2560 -h 1440 +map jump_mireal -noborder -console -novid +viewmodel_fov 130 >/dev/null 2>/dev/null &
+		/usr/bin/steam -silent -no-browser -offline -applaunch 440 -windowed -w 2560 -h 1440 +map jump_mireal -noborder -console -novid +viewmodel_fov 130 >/dev/null 2>/dev/null &
 		;;
 	3)
-		/usr/bin/steam -silent -no-browser -applaunch 440 -windowed -w 2560 -h 1440 +connect 46.174.48.183:27015 -novid -noborder -console +viewmodel_fov 130 >/dev/null 2>/dev/null &
+		/usr/bin/steam -silent -no-browser -offline -applaunch 440 -windowed -w 2560 -h 1440 +connect 46.174.48.183:27015 -novid -noborder -console +viewmodel_fov 130 >/dev/null 2>/dev/null &
 		;;
 	4)
 		echo
-		 /usr/bin/steam -silent -no-browser -applaunch 440 -windowed -w 2560 -h 1440 +connect 176.57.142.80:27055 -novid -noborder -console +viewmodel_fov 130 >/dev/null 2>/dev/null &
+		 /usr/bin/steam -silent -no-browser -offline -applaunch 440 -windowed -w 2560 -h 1440 +connect 176.57.142.80:27055 -novid -noborder -console +viewmodel_fov 130 >/dev/null 2>/dev/null &
 		;;
 	5)
 		echo
-		/usr/bin/steam -silent -no-browser -applaunch 440 -windowed -w 2560 -h 1440 +connect 5.135.189.29:27015  -novid -noborder -console +viewmodel_fov 130 >/dev/null 2>/dev/null &
+		/usr/bin/steam -silent -no-browser -offline -applaunch 440 -windowed -w 2560 -h 1440 +connect 5.135.189.29:27015  -novid -noborder -console +viewmodel_fov 130 >/dev/null 2>/dev/null &
 		;;
 	*)
 		echo 'ERR0R'
