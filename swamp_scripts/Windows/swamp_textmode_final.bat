c70fc005a31076d95aa68190ee8167bc01966a12
Pierre <49315644+ech1@users.noreply.github.com>
2021-01-07 11:20:19 +0100

    test new script
    (in case stupid steam tries to launch game without sync to cloud, it displays a warning and it just loops endlessly trying to launch gmod, so just kill steam automatically and start over, eventually steam connection will be back up and it will connect back)

--- swamp_scripts/Windows/swamp_textmode_final.bat
+++ swamp_scripts/Windows/swamp_textmode_final.bat
@@ -4,12 +4,13 @@
 
 Echo "Swamp Cinema Automation Script"
 Set COUNTER=0
+Set COUNTER2=0
 
 GOTO :A
 
 :A
 set /A COUNTER=COUNTER+1
-echo time is %COUNTER% limit is 1440 
+echo time is %COUNTER% limit is 720
 IF %COUNTER% gtr 720 (GOTO :killer)
 TASKLIST | FINDSTR /I "gmod.exe"
 IF ERRORLEVEL 1 (GOTO :StartSwamp) ELSE (ECHO Swamp Cinema is currently running)
@@ -21,15 +22,18 @@ ECHO "Swamp Cinema closed, Restarting..."
 REM "C:\Program Files (x86)\Steam\Steam.exe" -applaunch 4000 +connect cinema.swampservers.net:27015 -noaddons -nosrgb -nochromium -low -heapsize 1048576 -forcenovsync -hushasserts -noipx -nops2b -textmode -novid -nocdaudio -hijack -flushlog +r_hunkalloclightmaps 0 -mat_showlowresimage -processheap
 "C:\Program Files (x86)\Steam\Steam.exe" -applaunch 4000 +connect cinema.swampservers.net:27015 -textmode -low -noaddons -nochromium -novid -nosound
 timeout 60
+set /A COUNTER2=COUNTER2+1
+IF %COUNTER2% gtr 10 (GOTO :KillSteam)
 GOTO :A
 
-:StartSteam
-ECHO "Steam is closed, Restarting..."
-start Steam
-timeout 60
+:KillSteam
+taskkill /IM steam.exe /F
+Set COUNTER=0
+Set COUNTER2=0
 GOTO :A
 
 :killer
 taskkill /IM gmod.exe /F
 Set COUNTER=0
+Set COUNTER2=0
 GOTO :A
