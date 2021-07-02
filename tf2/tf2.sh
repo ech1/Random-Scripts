496bfeef4577fc7424e153a8e66962cae05a2213
Pierre <49315644+ech1@users.noreply.github.com>
2021-07-02 18:16:51 +0200

    Update tf2.sh

--- tf2/tf2.sh
+++ tf2/tf2.sh
@@ -14,27 +14,16 @@ case $choice in
         2)
                 /usr/bin/steam -silent -no-browser -offline -applaunch 440 -windowed -w 2560 -h 1440 +map airborne -noborder -console -novid +viewmodel_fov 130 >/dev/null 2>/dev/null &
                 echo 'mp_teams_unbalance_limit 30 ; mp_disable_respawn_times 1 ; tf_bot_add 3 soldier red expert ; tf_bot_force_class soldier'
-                echo 'mp_teams_unbalance_limit 30 ; mp_disable_respawn_times 1 ; tf_bot_add 3 soldier red expert ; tf_bot_force_class soldier ; sv_cheats 1 ; buddha'
+                
                 ;;
-        #3)
-        #       /usr/bin/steam -silent -no-browser -offline -applaunch 440 -windowed -w 2560 -h 1440 +map tr_rocket_shooting2 -noborder -console -novid +viewmodel_fov 130 >/dev/null 2>/dev/null &
-        #       echo 'bot_kick all; tf_bot_force_class soldier; tf_bot_add 9 red soldier expert ; buddha'
-        #       #/usr/bin/steam -silent -no-browser -offline -applaunch 440 -windowed -w 2560 -h 1440 +map tr_rocket_shooting2 -noborder -console -novid +viewmodel_fov 130 +bot_kick all tf_bot_force_class soldier tf_bot_add 3 red soldier expert >/dev/null 2>/dev/null &
-        #       ;;
+        3)
+               /usr/bin/steam -silent -no-browser -offline -applaunch 440 -windowed -w 2560 -h 1440 +map airborne -noborder -console -novid +viewmodel_fov 130 >/dev/null 2>/dev/null &
+               echo 'mp_teams_unbalance_limit 30 ; mp_disable_respawn_times 1 ; tf_bot_add 3 soldier red expert ; tf_bot_force_class soldier ; sv_cheats 1 ; buddha'
+        ;;
         *)
                 echo 'ERR0R'
                 break
                 ;;
 esac
 
-
-#temple map
-#] mp_teams_unbalance_limit 32
-#] mp_disable_respawn_times 1
-#] tf_bot_add 30 soldier blue
-
-#
-#
-#
-#
 
